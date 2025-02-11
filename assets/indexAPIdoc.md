e36595ae73dd3035eb89d22dfbfa40616e40059f
youwol-guillaume <reinisch.gui@gmail.com>
2025-02-11 09:47:13 +0100

    📝 Clean `typedoc.js` & provide custom `readme.md`

---
+++ assets/indexAPIdoc.md
@@ -0,0 +1,11 @@
+The **webpm-client** library provides a powerful and flexible way to manage and install components in both the main
+thread and Web Workers.
+
+*  The primary entry point is {@link MainModule}, which serves as the core module for installing components in the main 
+   thread.
+
+*  The {@link WorkersPoolModule} extends functionality by enabling component installation in Web Workers, as well as 
+  task execution and monitoring.
+
+Use this documentation to explore the available modules, APIs, and best practices for integrating **webpm-client** 
+into your project.
--- typedoc.js
+++ typedoc.js
@@ -1,30 +1,12 @@
 /* eslint-env node -- eslint-comment add exception because the running context is node environment */
 module.exports = {
     // The order below is intentional, it is related to symbols being re-exported.
-    entryPoints: ['./src/lib/workers-pool/index.ts', './src/index.ts'],
-    exclude: ['src/tests'],
-    intentionallyNotExported: [
-        'InstallModulesInputs',
-        'InstallScriptsInputs',
-        'InstallStyleSheetsInputs',
-        'StateImplementation',
+    entryPoints: [
+        './src/lib/workers-pool/index.ts', 
+        './src/index.ts'
     ],
+    exclude: ['src/tests'],
+    readme: './assets/indexAPIdoc.md',
     out: 'dist/docs',
     theme: 'default',
-    categorizeByGroup: false,
-    categoryOrder: [
-        'Getting Started',
-        'Entry Points',
-        'Loading screen',
-        'State',
-        'WorkersPool',
-        'Worker Environment',
-        "Client's method inputs",
-        'Events',
-        "Worker's Message",
-        'Errors',
-        'View',
-        'Helpers',
-        '*',
-    ],
 }
